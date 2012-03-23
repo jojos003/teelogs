51f501204682b99213bb75ad8578a072c9ac2412
jonathan <jgeorges@advancedsolutionsaccelerator.com>
2012-03-23 14:36:17 +0100

    Sors désormais les infos suivantes :   - qui à tué qui et combien de fois   - le nombre totale de victoires, suicides et défaites
    Déplacement du fichier de log de test vers un répertoire approprié.

--- teelogs.rb
+++ teelogs.rb
@@ -31,24 +31,76 @@ def parse_values(str)
     keys = str.scan(/\w+=/)
     keys.each do |key|
         i = str.index(key) + key.length
-        ni = i
-        if(str[i] =~ /'|"/)
-            ni = str.index(/'|"/, i+1)
-        elsif(i+1 < str.length)
-            ni = str.index(/ /, i+1)
+        ni = i+1
+        if(str[i] =~ /['"]/)
+            i = i+1
+            ni = str.index(/['"]/, ni)
+        elsif(ni < str.length)
+            ni = str.index(/ /, ni)
         end
-        values[key.chop] = str.slice(i, ni)
+        values[key.chop] = str[i...ni]
     end
     values
+end
+
+def clean_name(str)
+    str.sub(/\A\d+:/, '')
+end
+
+def results(kills)
+    results = Hash.new
+
+    kills.each do |kill|
+        values = parse_values(kill)
+        killer = clean_name(values["killer"])
+        victim = clean_name(values['victim'])
+        
+        results[killer] = Hash.new unless results[killer]
+        results[killer][victim] = unless results[killer][victim]
+            1
+        else
+            results[killer][victim] + 1
+        end
+    end
+    results
+end
+
+def scores(results)
+    scores = Array.new
+    results.each do |key, value|
+        kill = 0
+        suicide = 0
+        victim = 0
+        value.each do |k, v| 
+            if key == k
+                suicide = suicide + v
+            else
+                kill = kill + v
+            end
+        end
+        
+        results.each do |k, v|
+            next if key == k
+            v.each {|k2,v2| victim = victim + v2 if key == k2 }
+        end
+        
+        s = {kill: kill, suicide: suicide, victim: victim}
+        scores << {key => s}
+    end
+    scores
 end
 
 logs = parse_logs(open(*ARGV))
 
 kills = actions(logs, :kill)
+results = results(kills)
+pp results
+scores = scores(results)
+pp scores
 
-results = Hash.new
 
-kills.each do |kill|
-    pp parse_values(kill)
-end
+
+
+
+
 
